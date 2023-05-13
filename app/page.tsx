763ffbb01a420769645003f1d4dfc529d41f05ff
Khant Sithu <khantseithuu@gmail.com>
2023-05-13 10:26:54 +0000

    rm: redirect

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,5 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await getServerSession();
-  console.log(session);
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
   return <main></main>;
 }
