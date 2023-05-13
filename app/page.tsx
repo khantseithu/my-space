4618037ab25b8d10815b72b95781b9f4a520e5d4
Khant Sithu <khantseithuu@gmail.com>
2023-05-13 10:26:33 +0000

    ref: protect routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    null
-  )
+export default async function Home() {
+  const session = await getServerSession();
+  console.log(session);
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+  return <main></main>;
 }
