7e6e1f0c2516724304ff63d2df93e80ac69c67d2
Khant Sithu <khantseithuu@gmail.com>
2023-05-13 09:06:52 +0000

    build nav menu

---
+++ app/NavMenu.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "./NavMenu.module.css";
+import Image from "next/image";
+
+const NavMenu = () => {
+  return (
+    <nav className={styles.nav}>
+      <Link href={"/"}>
+        <Image alt="Next space logo" src="/logo.svg" width={216} height={30} />
+      </Link>
+      <ul className={styles.links}>
+        <li>
+          <Link href={"/about"}>About</Link>
+        </li>
+        <li>
+          <Link href={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link href={"/users"}>Users</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default NavMenu;
